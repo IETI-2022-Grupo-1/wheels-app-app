534190c3e4b23b09275dd405da9b0a9237b06f06
Juan Cadavid <53835422+jua1000n@users.noreply.github.com>
2022-11-27 13:43:00 -0500

    Revert "HomeDriver, HomePassenger"
    This reverts commit 936576249473ce46be1c5c8b1ad99f7d3d89fe2b.

--- app/src/main/java/com/example/wheelsapp/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/wheelsapp/ui/dashboard/DashboardFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -16,20 +18,20 @@ public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
-
+    private CardView cardView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+       //         new ViewModelProvider(this).get(DashboardViewModel.class);
 
-
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
+
+
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
         return root;
     }
 
