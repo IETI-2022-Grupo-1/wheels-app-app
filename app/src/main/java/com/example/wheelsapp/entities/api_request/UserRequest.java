99f2238254baf3c52f6b338ba3c38f0215e37a59
Your Name <you@example.com>
2022-11-30 17:37:29 -0500

    apiEntities

--- app/src/main/java/com/example/wheelsapp/entities/api_request/UserRequest.java
+++ app/src/main/java/com/example/wheelsapp/entities/api_request/UserRequest.java
@@ -1,6 +1,7 @@
 package com.example.wheelsapp.entities.api_request;
 
 public class UserRequest {
+
     private String name;
     private String lastName;
     private String email;
@@ -13,6 +14,7 @@ public class UserRequest {
     public UserRequest() {}
 
     public UserRequest(String name, String lastName, String email, String password, Integer phoneNumber, String city, String profilePhoto) {
+
         this.name = name;
         this.lastName = lastName;
         this.email = email;
@@ -26,63 +28,48 @@ public class UserRequest {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public Integer getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(Integer phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
     public String getOrganization() {
         return organization;
     }
-
     public void setOrganization(String organization) {
         this.organization = organization;
     }
-
     public String getProfilePhoto() {
         return profilePhoto;
     }
-
     public void setProfilePhoto(String profilePhoto) {
         this.profilePhoto = profilePhoto;
     }
--- app/src/main/java/com/example/wheelsapp/entities/api_request/VehiRequest.java
+++ app/src/main/java/com/example/wheelsapp/entities/api_request/VehiRequest.java
@@ -1,8 +1,8 @@
 package com.example.wheelsapp.entities.api_request;
 
-import java.util.Date;
 
 public class VehiRequest {
+
     private String idUser;
     private String soat;
     private Integer puestos;
@@ -16,6 +16,7 @@ public class VehiRequest {
     }
 
     public VehiRequest(String idUser, Integer puestos, String description, String model) {
+
         this.idUser = idUser;
         this.soat = "SOAT TBD";
         this.puestos = puestos;
@@ -29,63 +30,48 @@ public class VehiRequest {
     public String getIdUser() {
         return idUser;
     }
-
     public void setIdUser(String idUser) {
         this.idUser = idUser;
     }
-
     public String getSoat() {
         return soat;
     }
-
     public void setSoat(String soat) {
         this.soat = soat;
     }
-
     public Integer getPuestos() {
         return puestos;
     }
-
     public void setPuestos(Integer puestos) {
         this.puestos = puestos;
     }
-
     public String getPropertyCard() {
         return propertyCard;
     }
-
     public void setPropertyCard(String propertyCard) {
         this.propertyCard = propertyCard;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getPhoto() {
         return photo;
     }
-
     public void setPhoto(String photo) {
         this.photo = photo;
     }
-
     public boolean isActive() {
         return isActive;
     }
-
     public void setActive(boolean active) {
         isActive = active;
     }
-
     public String getModel() {
         return model;
     }
-
     public void setModel(String model) {
         this.model = model;
     }
