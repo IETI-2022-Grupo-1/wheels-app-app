99e57b6e7c73ac8ebcb5ccacbc9c7646e0857c65
Juan Murillo <jdmurillo@google.com>
2022-12-01 07:53:10 -0500

    Activities modified

--- app/src/main/java/com/example/wheelsapp/PopUpRegisterUser.java
+++ app/src/main/java/com/example/wheelsapp/PopUpRegisterUser.java
@@ -26,7 +26,6 @@ public class PopUpRegisterUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up_register_user);
 
